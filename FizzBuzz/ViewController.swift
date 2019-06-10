9471ecddeaf60dc20bfd22a065c9b1520a6e20e4
Chenbo Yi <yichenbo@outlook.com>
2019-06-10 20:35:03 +0100

    Initial Commit

---
+++ FizzBuzz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FizzBuzz
+//
+//  Created by Simon 易 on 10/06/2019.
+//  Copyright © 2019 SimonChenboYi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
